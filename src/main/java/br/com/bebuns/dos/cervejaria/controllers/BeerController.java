31e2baa0a84332cba47aa176074191f7f4544949
Gustavo Pinheiro <mrpine19@gmail.com>
2026-05-05 20:13:33 -0300

    feat: criando os métodos gets para Beer

--- src/main/java/br/com/bebuns/dos/cervejaria/controllers/BeerController.java
+++ src/main/java/br/com/bebuns/dos/cervejaria/controllers/BeerController.java
@@ -1,11 +1,30 @@
 package br.com.bebuns.dos.cervejaria.controllers;
 
+import br.com.bebuns.dos.cervejaria.models.Beer;
+import br.com.bebuns.dos.cervejaria.services.BeerService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/beers")
 @RequiredArgsConstructor
 public class BeerController {
+
+    private final BeerService beerService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<Beer> getAllBeer(){
+        return beerService.getAllBeers();
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public Beer getBeerById(@RequestParam Long id){
+        return beerService.findById(id);
+    }
+
 }
--- src/main/java/br/com/bebuns/dos/cervejaria/repositorys/BeerRepository.java
+++ src/main/java/br/com/bebuns/dos/cervejaria/repositorys/BeerRepository.java
@@ -4,4 +4,5 @@ import br.com.bebuns.dos.cervejaria.models.Beer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BeerRepository extends JpaRepository<Beer, Long> {
+    Beer findByBreweryId(Long id);
 }

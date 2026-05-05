e0f2489580a7f0e7a3c8d9f90f8be41f51ec3312
Gustavo Pinheiro <mrpine19@gmail.com>
2026-05-05 19:49:00 -0300

    feat: criando BeerRepository

---
+++ src/main/java/br/com/bebuns/dos/cervejaria/repositorys/BeerRepository.java
@@ -0,0 +1,7 @@
+package br.com.bebuns.dos.cervejaria.repositorys;
+
+import br.com.bebuns.dos.cervejaria.models.Beer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BeerRepository extends JpaRepository<Beer, Long> {
+}

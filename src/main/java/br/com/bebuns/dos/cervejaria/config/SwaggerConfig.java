cfe669cfbeb294bdb2e6cc34a8af46ced98fdefd
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-05-05 20:35:01 -0300

    feat: swagger config

---
+++ src/main/java/br/com/bebuns/dos/cervejaria/config/SwaggerConfig.java
@@ -0,0 +1,16 @@
+package br.com.bebuns.dos.cervejaria.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Beer Guide API",
+                version = "1.0",
+                description = "API para Guia de Cervejas Artesanais, para os apreciadores."
+        )
+)
+public class SwaggerConfig {
+}

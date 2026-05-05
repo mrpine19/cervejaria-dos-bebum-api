972c5ae482e90731dc3810adcb7e467f981959ae
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-05-05 20:58:44 -0300

    feat: cacheable brewery

--- src/main/java/br/com/bebuns/dos/cervejaria/services/BreweryService.java
+++ src/main/java/br/com/bebuns/dos/cervejaria/services/BreweryService.java
@@ -4,6 +4,8 @@ import br.com.bebuns.dos.cervejaria.models.Beer;
 import br.com.bebuns.dos.cervejaria.models.Brewery;
 import br.com.bebuns.dos.cervejaria.repositorys.BreweryRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -18,18 +20,22 @@ public class BreweryService {
 
     private final BreweryRepository breweryRepository;
 
+    @Cacheable(value = "breweries")
     public Page<Brewery> listAllBreweries(Pageable pageable){
         return breweryRepository.findAll(pageable);
     }
 
+    @Cacheable(value = "brewery")
     public Optional<Brewery> findByIdBrewery(Long id){
         return breweryRepository.findById(id);
     }
 
+    @CacheEvict(value = {"brewery", "breweryByBrewery", "brewery"})
     public Brewery createBrewery(Brewery brewery){
         return breweryRepository.save(brewery);
     }
 
+    @CacheEvict(value = {"brewery", "breweryByBrewery", "brewery"})
     public Brewery update(Long id, Brewery updatedBrewery) {
         Brewery existingBrewery = breweryRepository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Brewery not found :(")
@@ -40,6 +46,7 @@ public class BreweryService {
         return breweryRepository.save(existingBrewery);
     }
 
+    @CacheEvict(value = {"brewery", "breweryByBrewery", "brewery"}, allEntries = true)
     public void delete(Long id) {
         Brewery existingBrewery = breweryRepository.findById(id).orElseThrow(
                 () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Beer not found :(")

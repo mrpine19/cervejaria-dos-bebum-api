87150f5884c92efdfb7d981bb7927b81a275ed75
Gustavo Pinheiro <mrpine19@gmail.com>
2026-05-05 19:57:49 -0300

    feat: criando BeerRepository

---
+++ src/main/java/br/com/bebuns/dos/cervejaria/controllers/BeerController.java
@@ -0,0 +1,10 @@
+package br.com.bebuns.dos.cervejaria.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/beers")
+@RequiredArgsConstructor
+public class BeerController {
+}
